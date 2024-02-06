c364deef5486c02b763564fcd368986612cd6f08
AlejandroLozano06 <5609124@alu.murciaeduca.es>
2024-02-06 19:53:11 +0100

    Create Practica4_1.java

---
+++ Practica4_1.java
@@ -0,0 +1,11 @@
+import java.time.LocalDateTime;
+public class Practica4_1 {
+  public static void main(String[] args){
+    System.out.println("Hola mundo");
+  }
+
+public static void mostrarFechaYHora(){
+  LocalDateTime fechaHoraActual = LocalDateTime.now(){
+    System.out.println("Fecha y hora actual: " + fechaHoraActual);
+  }
+}
